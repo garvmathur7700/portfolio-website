8a8fb208f0c39db7d8f5566ba7de56eb67f0f10f
Garv Mathur <garvmathur7700@gmail.com>
2025-09-01 04:25:45 +0530

    configured next.config.ts for deployment on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,19 @@
-import type {NextConfig} from 'next';
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
   },
+  basePath: isProd ? '/portfolio-website' : '',
+  assetPrefix: isProd ? '/portfolio-website/' : '',
   images: {
+    unoptimized: true, // required for GitHub Pages
     remotePatterns: [
       {
         protocol: 'https',
